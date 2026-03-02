674da9c7164d5ac41f975e20581e5067a93d6d51
SuvamRay <suvamray5@gmail.com>
2026-03-02 18:13:57 +0530

    Initial commit - Maven project

---
+++ src/main/java/com/monk_commerce/coupons/App.java
@@ -0,0 +1,13 @@
+package com.monk_commerce.coupons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
